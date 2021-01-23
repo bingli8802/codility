85857a57f19cb6c6cc883b28b18cdce75ee221b1
bingli8802 <68235578+bingli8802@users.noreply.github.com>
2021-01-22 23:32:05 -0800

    Update 4.2_MaxCounters.py

--- 4.2_MaxCounters.py
+++ 4.2_MaxCounters.py
@@ -11,3 +11,20 @@ def solution(N, A):
         else:
             arr = [max(arr)] * N
     return arr
+
+# O(N+M)
+def solution(N, A):
+    # write your code in Python 3.6
+    counters = [0] * N
+    max_counter = 0
+    last_update = 0
+    for a in A:
+        if a != N + 1:
+            counters[a-1] = max(counters[a-1], last_update)
+            counters[a-1] += 1
+            max_counter = max(counters[a-1], max_counter)
+        else:
+            last_update = max_counter
+    for i in range(N):
+        counters[i] = max(counters[i], last_update)
+    return counters
